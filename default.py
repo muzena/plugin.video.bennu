c54aca1cb6f3e75f47e150926d7bb3b75d452b69
Colossal1 <colossus_repo@gmx.com>
2017-10-09 17:47:51 +0100

    Bump to 17.10.9

--- default.py
+++ default.py
@@ -28,15 +28,36 @@ content = params.get('content')
 
 name = params.get('name')
 
+title = params.get('title')
+
 url = params.get('url')
 
 image = params.get('image')
 
 fanart = params.get('fanart')
 
+source = params.get('source')
+
+try: worker = params.get('worker')
+except: worker = '0'
+
+windowedtrailer = params.get('windowedtrailer')
+windowedtrailer = int(windowedtrailer) if windowedtrailer in ("0","1") else 0
+
 if action == None:
     from resources.lib.indexers import bennustreams
     bennustreams.indexer().root()
+    
+    try:
+        import xbmc,xbmcaddon
+        import common as Common
+
+        ADDON = xbmcaddon.Addon(id='plugin.video.bennu')
+
+        if ADDON.getSetting('first.info') == "true":
+            TypeOfMessage="t"; (NewImage,NewMessage)=Common.FetchNews()
+            Common.CheckNews(TypeOfMessage,NewImage,NewMessage,False)
+    except: pass
     
 elif action == 'directory':
     from resources.lib.indexers import bennustreams
@@ -48,8 +69,21 @@ elif action == 'qdirectory':
 
 elif action == 'xdirectory':
     from resources.lib.indexers import bennustreams
-    bennustreams.indexer().getx(url)
+    if worker == '1': bennustreams.indexer().getx(url, worker=True)
+    else: bennustreams.indexer().getx(url)
 
+elif action == 'imdb_list':
+    from resources.lib.indexers import bennustreams
+    bennustreams.indexer().getimdb(url)
+
+elif action == 'trakt_list':
+    from resources.lib.indexers import bennustreams
+    bennustreams.indexer().gettrakt(url)
+    
+elif action == 'rotten_list':
+    from resources.lib.indexers import bennustreams
+    bennustreams.indexer().getrotten(url)
+    
 elif action == 'developer':
     from resources.lib.indexers import bennustreams
     bennustreams.indexer().developer()
@@ -73,6 +107,14 @@ elif 'youtube' in str(action):
 elif action == 'play':
     from resources.lib.indexers import bennustreams
     bennustreams.player().play(url, content)
+    
+elif action == 'addItem':
+    from resources.lib.modules import sources
+    sources.sources().addItem(title)
+    
+elif action == 'playItem':
+    from resources.lib.modules import sources
+    sources.sources().playItem(title, source)
 
 elif action == 'browser':
     from resources.lib.indexers import bennustreams
@@ -107,41 +149,53 @@ elif action == 'addView':
     views.addView(content)
 
 elif action == 'downloader':
-    from resources.lib.modules import downloader
-    downloader.downloader()
+    from resources.lib.modules import downloader_bennu
+    downloader_bennu.downloader()
 
 elif action == 'addDownload':
-    from resources.lib.modules import downloader
-    downloader.addDownload(name,url,image)
+    from resources.lib.modules import downloader_bennu
+    downloader_bennu.addDownload(name,url,image)
 
 elif action == 'removeDownload':
-    from resources.lib.modules import downloader
-    downloader.removeDownload(url)
+    from resources.lib.modules import downloader_bennu
+    downloader_bennu.removeDownload(url)
 
 elif action == 'startDownload':
-    from resources.lib.modules import downloader
-    downloader.startDownload()
+    from resources.lib.modules import downloader_bennu
+    downloader_bennu.startDownload()
 
 elif action == 'startDownloadThread':
-    from resources.lib.modules import downloader
-    downloader.startDownloadThread()
+    from resources.lib.modules import downloader_bennu
+    downloader_bennu.startDownloadThread()
 
 elif action == 'stopDownload':
-    from resources.lib.modules import downloader
-    downloader.stopDownload()
+    from resources.lib.modules import downloader_bennu
+    downloader_bennu.stopDownload()
 
 elif action == 'statusDownload':
-    from resources.lib.modules import downloader
-    downloader.statusDownload()
+    from resources.lib.modules import downloader_bennu
+    downloader_bennu.statusDownload()
 
 elif action == 'trailer':
     from resources.lib.modules import trailer
-    trailer.trailer().play(name)
+    trailer.trailer().play(name, windowedtrailer=windowedtrailer)
 
 elif action == 'clearCache':
     from resources.lib.indexers import navigator
     navigator.navigator().clearCache()
 
+elif action == 'clearCacheMeta':
+    from resources.lib.indexers import navigator
+    navigator.navigator().clearCacheMeta()
+  
+elif action == 'clearCacheProviders':
+    from resources.lib.indexers import navigator
+    navigator.navigator().clearCacheProviders()
+
+elif action == 'clearCacheAll':
+    from resources.lib.indexers import navigator
+    navigator.navigator().clearCacheAll()
+    
 elif action == 'radios':
     from resources.lib.indexers import phradios
     phradios.radios()
